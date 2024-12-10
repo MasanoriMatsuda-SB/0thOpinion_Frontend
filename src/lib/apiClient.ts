30e04db97d9aab0050d2128b8c195fcf083cf4d6
M-Matsuda <masanori.matsuda.sb@gmail.com>
2024-12-10 22:37:56 +0900

    デプロイ用の修正。

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://tech0-gen-8-step3-app-py-9.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
